9acd283c0145eed7bc50fd24f11b91213d2d667c
Jun <jwanautomation@gmail.com>
2023-05-14 10:34:30 +0100

    feat: Updated getComputerChoice - Function logs rock, paper or scissors randomly after refreshing

--- js/main.js
+++ js/main.js
@@ -5,6 +5,15 @@
 
 */
 
-const getComputerChoice = () => Math.floor(Math.random() * 3) + 1;
+const getComputerChoice = () => {
+  const randomInt = Math.floor(Math.random() * 3) + 1;
 
+  if (randomInt === 1) {
+    return 'Rock';
+  } else if (randomInt === 2) {
+    return 'Paper';
+  } else if (randomInt === 3) {
+    return 'Scissors';
+  }
+}
  console.log(getComputerChoice());
